3798ab754287c6334b2e78619c1be0430504a29f
Fedor <fedorinjava@gmail.com>
2022-10-23 21:53:55 +0300

    I solved my homework once

--- lesson1/src/main/java/Task1.java
+++ lesson1/src/main/java/Task1.java
@@ -1,5 +1,9 @@
-public class Task1 {
-    public static void main(String[] args) {
+class Hello
+{
+    public static void main(String[] args)
+    {
         System.out.println("My first program");
     }
 }
+
+
