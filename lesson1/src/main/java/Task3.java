3453cac2890207230a9573b6e565f79d6b07479d
Fedor <fedorinjava@gmail.com>
2022-10-30 10:30:29 +0300

    Task3 correct

---
+++ lesson1/src/main/java/Task3.java
@@ -0,0 +1,13 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int count = 0;
+        for (String arg : args) {
+            int value = Integer.parseInt(arg);
+            if (value > 0) {
+                count = count + 1;
+            }
+        }
+
+        System.out.println("Количество положительных чисел" + count);
+    }
+}
