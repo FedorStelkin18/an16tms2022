91bea87884552968fd828c8fa0054e86d1a3c916
Fedor <fedorinjava@gmail.com>
2022-11-20 18:04:50 +0300

    ДЗ № 4

--- untitled/src/main/java/Alfavit.java
+++
@@ -1,8 +0,0 @@
-public class Alfavit {
-    public static void main(String[] args) {
-        //вывести на консоль все буквы английского алфавита
-        for (char i = 'a'; i <= 'z'; ++i) {
-            System.out.println(i);
-        }
-    }
-}
